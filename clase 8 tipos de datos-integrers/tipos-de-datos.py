1e678cfa6bec18364775489ec10d35c84f60982d
pana_miguel <miguelsierra1512@gmail.com>
2024-12-24 11:54:50 -0500

    clase 8.

---
+++ clase 8 tipos de datos-integrers/tipos-de-datos.py
@@ -0,0 +1,30 @@
+#esta variable que siempre es la misma cambia de tipo de dato segun lo que se nesecite en el moemnto esto solo se puede 
+#realizar en python ya que es un lenguaje que no es fuertemente tipado como lo puede ser java al cual toca especificar sutipo de dato 
+
+# Definimos una variable con un valor entero
+variable = 42
+print(f"El valor es {variable} tipo de dato es {type(variable)}")
+
+# Cambiamos la variable a un valor de tipo cadena (str)
+variable = "Juan Perez"
+print(f"El valor es {variable} tipo de dato es {type(variable)}")
+
+# Cambiamos la variable a un valor de tipo flotante (float)
+variable = 3.14
+print(f"El valor es {variable} tipo de dato es {type(variable)}")
+
+# Cambiamos la variable a un valor booleano (bool)
+variable = True
+print(f"El valor es {variable} tipo de dato es {type(variable)}")
+
+# Cambiamos la variable a una lista
+variable = ["Ana", "Luis", "Pedro"]
+print(f"El valor es {variable} tipo de dato es {type(variable)}")
+
+# Cambiamos la variable a un diccionario
+variable = {"nombre": "Maria", "edad": 30}
+print(f"El valor es {variable} tipo de dato es {type(variable)}")
+
+# Cambiamos la variable a un valor booleano (otra vez)
+variable = False
+print(f"El valor es {variable} tipo de dato es {type(variable)}")
