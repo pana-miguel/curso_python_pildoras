68c6f6f957dd81410065a20bab7c5d25016dbc69
pana_miguel <miguelsierra1512@gmail.com>
2024-12-30 11:08:11 -0500

    clase 23.

---
+++ clase 23 for loop/for-loop.py
@@ -0,0 +1,9 @@
+#iterables estos son los elementos que se pueden recorrer uno por uno en un ciclo for 
+#esto funciona con listas y tuplas y tambien con los sets y diccionarios el cual los recorrera por sus claves 
+#y en el caso de el set lo recorrera de forma ordenada pero puede que al imprimirlo no este ordnada por du forma de buscar los elementos 
+
+listaNombre = ['juan', 'camilo', 'andres', 'jose']
+
+for nombre in listaNombre:
+    print(nombre) 
+
